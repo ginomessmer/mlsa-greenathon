f2e477253dc3894cf7bedb9ad7b10b6dca497811
Gino Messmer <git@ginomessmer.me>
2021-04-27 21:28:09 +0200

    Support all map styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,30 @@ import React, { useEffect, useState } from 'react';
 import Modal from 'react-modal';
 
 import { Sidebar, SearchBox, SubmitBusinessForm, ModalCloseButton } from './components';
-import { queryBusiness, submitBusiness } from './services/api';
+import { Business, queryBusiness, submitBusiness } from './services/api';
 
-import { AzureMap, AzureMapFeature, AzureMapsProvider, AzureMapDataSourceProvider, AzureMapLayerProvider } from 'react-azure-maps';
-import { AuthenticationType, data } from 'azure-maps-control';
+import { AzureMap, AzureMapFeature, AzureMapsProvider, AzureMapDataSourceProvider, AzureMapLayerProvider, IAzureMapLayerType, IAzureMapControls, IAzureMapOptions } from 'react-azure-maps';
+import { AuthenticationType, data, control, ControlOptions } from 'azure-maps-control';
 
 import './App.scss';
 
-const azureMapOptions = {
+const azureMapOptions: IAzureMapOptions = {
   authOptions: {
     authType: AuthenticationType.subscriptionKey,
     subscriptionKey: 'Ea_uzBbOhHmVPM5UjV-vEoW7wRw0bO-RTc7bo7AhtIw'
   },
+}
+
+const azureMapControls: IAzureMapControls[] = [
+  {
+    controlName: 'StyleControl',
+    controlOptions: { mapStyles: 'all' },
+    options: { position: 'top-right' } as ControlOptions
+  }
+]
+
+export interface BusinessMarker {
+  position: data.Position;
 }
 
 Modal.setAppElement('#root');
@@ -24,15 +36,15 @@ const App = () => {
   const closeModal = () => setSubmitBusinessModalOpen(false);
   
   // Map configuration
-  const [markersLayer] = useState('SymbolLayer');
+  const [markersLayer] = useState<IAzureMapLayerType>('SymbolLayer');
 
   // Businesses
-  const [businesses, setBusinesses] = useState([]);
+  const [businesses, setBusinesses] = useState<BusinessMarker[]>([]);
 
   useEffect(() => {
     queryBusiness()
       .then(x => {
-        var collection = x.data.map(z => ({
+        var collection = x.data.map((z: Business) => ({
           position: new data.Position(z.longitude, z.latitude)
         }))
 
@@ -40,7 +52,7 @@ const App = () => {
       });
   }, []);
 
-  const handleSubmitBusiness = (values) => {
+  const handleSubmitBusiness = (values: any) => {
     submitBusiness(values)
       .then(() => {
         alert('Thanks for submitting your business. Your business will be displayed on the map once it has been approved by the website owners.');
@@ -54,7 +66,8 @@ const App = () => {
       <AzureMapsProvider>
         <div>
           <div className="map-container">
-            <AzureMap options={azureMapOptions}>
+            <AzureMap options={azureMapOptions}
+              controls={azureMapControls}>
               <AzureMapDataSourceProvider id={'markers AzureMapDataSourceProvider'} options={{ cluster: true, clusterRadius: 2 }}>
                 <AzureMapLayerProvider
                   id={'markers AzureMapLayerProvider'}
