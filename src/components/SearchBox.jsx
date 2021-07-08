73bde656636159de0b9b9939f081b1dad7f3025a
Gino Messmer <git@ginomessmer.me>
2021-07-08 21:42:32 +0200

    Move mock data and hide auto complete when term is empty

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import JSONDATA from "./MOCK_DATA.json"
+import JSONDATA from "../data/MOCK_DATA.json"
 import './SearchBox.scss';
-import {useState} from 'react';
+import { useState } from 'react';
+
 function SearchBox() {
-const [searchTerm, setSearchTerm] = useState('')
+  const [searchTerm, setSearchTerm] = useState('')
 
   return (
     <div className="search-box">
-      <input type="text" name="searchBox" id="searchBox" placeholder="Search a business..." onChange={event => {setSearchTerm(event.target.value)}}/>
-      {JSONDATA.filter((val)=> {
+      <input type="text" name="searchBox" id="searchBox"
+        placeholder="Search a business..."
+        onChange={event => { setSearchTerm(event.target.value) }} />
+      {searchTerm && JSONDATA.filter((val)=> {
         if (searchTerm == ""){
           return val
         } else if (val.title.toLowerCase().includes(searchTerm.toLowerCase())){
@@ -16,10 +19,10 @@ const [searchTerm, setSearchTerm] = useState('')
         }
       }).map((val,key)=>{
         return(
-         <div className="user" key={key}>
-         <p>{val.title}</p>
-         </div>
-       );
+        <div className="user" key={key}>
+          <p>{val.title}</p>
+        </div>
+      );
       })}
       </div>
   );
