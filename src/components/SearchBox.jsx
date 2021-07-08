0cb9f1178358b03696866e129f70f6e667205e27
Gino Messmer <git@ginomessmer.me>
2021-07-08 21:55:21 +0200

    Add overflow to search results box

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -11,19 +11,20 @@ function SearchBox() {
       <input type="text" name="searchBox" id="searchBox"
         placeholder="Search a business..."
         onChange={event => { setSearchTerm(event.target.value) }} />
-      {searchTerm && JSONDATA.filter((val)=> {
-        if (searchTerm == ""){
-          return val
-        } else if (val.title.toLowerCase().includes(searchTerm.toLowerCase())){
-          return val
-        }
-      }).map((val,key)=>{
-        return(
-        <div className="user" key={key}>
-          <p>{val.title}</p>
-        </div>
-      );
-      })}
+      
+      {searchTerm && <ul className="search-results">
+        {JSONDATA.filter((val)=> {
+          if (searchTerm == ""){
+            return val
+          } else if (val.title.toLowerCase().includes(searchTerm.toLowerCase())){
+            return val
+          }
+        }).map((val,key)=>{
+          return(
+            <li key={key}>{val.title}</li>
+          );
+        })}
+      </ul>}
       </div>
   );
 }
